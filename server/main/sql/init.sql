a7c1c766f8bcb28c3e22a928e1ffe8c568397852
kravcc <kykoro228@gmail.com>
2022-11-24 01:57:44 +0100

    db

--- server/main/sql/init.sql
+++ server/main/sql/init.sql
@@ -7,8 +7,8 @@ INSERT INTO main_person (id_person, firstname, surname, address, telephone, emai
     VALUES (2, 'Tomas', 'Vojnar', 'Kolejni 2, 612 00 Brno', '99999998', 'jatomas@mail.com', 'g');
 
 
-INSERT INTO main_course (id_course, abbrv, title, description, credits, max_persons, approved, type)
-    VALUES (1, 'IOS', 'Operační systémy', 'Cílem je seznámit studenty s principy operačních systémů obecně a dále se základy operačního systému Unix.', 5, 100, TRUE, 's');
+INSERT INTO main_course (id_course, abbrv, title, description, credits, max_persons, approved, type, id_person)
+    VALUES (1, 'IOS', 'Operační systémy', 'Cílem je seznámit studenty s principy operačních systémů obecně a dále se základy operačního systému Unix.', 5, 100, 1, 's', 2);
 
 
 INSERT INTO main_termin (id_termin, name, type, id_classroom, id_course)
